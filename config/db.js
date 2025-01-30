56ef86c060a9b7bc3e801eb47e3f3dcd87857f32
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2025-01-30 16:01:19 +0000

    sequelize connection changed to url

--- config/db.js
+++ config/db.js
@@ -1,11 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging: false, // Disable logging SQL queries
+// });
+
+
+const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
-    logging: false, // Disable logging SQL queries
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
+
+
+
 
 const connectDB = async () => {
     try {
